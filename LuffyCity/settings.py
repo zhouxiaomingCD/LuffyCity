0cb106cfc4b517e334f2c4630a406802da8bf50d
zhouxiaoming <1258416887@qq.com>
2019-11-27 16:41:21 +0800

    使用中间件解决跨域

--- LuffyCity/settings.py
+++ LuffyCity/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.CORS-Middleware.MyCors',
 ]
 
 ROOT_URLCONF = 'LuffyCity.urls'
